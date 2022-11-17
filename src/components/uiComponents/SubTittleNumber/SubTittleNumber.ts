a6d1a574e1ea426acbad1b9b509e8e8e71b86984
Iago Guinossi <iagoguinossi@gmail.com>
2022-11-17 18:56:50 -0300

    Create ui component

---
+++ src/components/uiComponents/SubTittleNumber/SubTittleNumber.ts
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+import { SubTittle } from "../SubTittle/SubTittle";
+
+export const SubTittleNumber = styled(SubTittle)`
+font-weight:700;
+margin-right: 28px;
+mix-blend-mode: normal;
+opacity: 0.25;
+`
