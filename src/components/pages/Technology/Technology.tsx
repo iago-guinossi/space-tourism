3fdc13b3a9ea0289cc5686b8e2cbf19f0ee4107b
Iago Guinossi <iagoguinossi@gmail.com>
2022-11-18 17:35:48 -0300

    refactor

--- src/components/pages/Technology/Technology.tsx
+++ src/components/pages/Technology/Technology.tsx
@@ -1,12 +1,16 @@
+import { useState } from "react";
 import styled from "styled-components";
 import { ContainerBackgroundImage } from "../../uiComponents/ContainerBackgroundImage";
 import { SubTittle } from "../../uiComponents/SubTittle";
 import { SubTittleNumber } from "../../uiComponents/SubTittleNumber";
-import { Text } from "../../uiComponents/Text";
-import { Tittle } from "../../uiComponents/Tittle";
 import { Capsule } from "./Capsule";
 import { Spaceport } from "./Spaceport";
 import { Vehicle } from "./Vehicle";
+
+type RoundedButtonProps = {
+    number: number;
+    state: number;
+}
 
 const SubTittleContainer = styled.div`
   display: flex;
@@ -20,55 +24,55 @@ const ConteudoContainer = styled.div`
   align-items: center;
 `;
 
-const RoundedButton = styled.button`
-  width: 80px;
-  height: 80px;
-  border: 1px solid rgba(255, 255, 255, 0.25);
-  border-radius: 100%;
-  background-color: transparent;
-  margin-bottom: 32px;
-  `;
 
 const ButtonContainer = styled.div`
   display: flex;
   flex-direction: column;
   margin-right: 80px;
   margin-bottom: -32px;
-`;
+  `;
 
 const NumberButton = styled.span`
   font-family: "Bellefair";
   font-size: 32px;
   line-height: 37px;
   letter-spacing: 2px;
-  color: #ffffff;
-`;
+  `;
 
 const MainContainer = styled.div`
   display: flex;
   flex-direction: column;
   margin: 0 auto;
-`;
+  `;
 
-const TextContainer = styled.div`
-  display: flex;
-  flex-direction: column;
-  width: 470px;  
-  margin-right: 130px;
-`;
-
-const SubTextContainer = styled.div`
-margin-top: 17px;
-width: 444px;
-`
-
-const Img = styled.img`
-width:515px;
-height: 527px;
-margin-right: -164px;
-`
+  const RoundedButton = styled.button<RoundedButtonProps>`
+    width: 80px;
+    height: 80px;
+    border: 1px solid rgba(255, 255, 255, 0.25);
+    border-radius: 100%;
+    background-color: ${({number, state})=> number === state ? '#FFFFFF' : 'transparent'};
+    color: ${({number, state})=> number === state ? 'rgba(11, 13, 23, 1)' : '#FFFFFF'};
+    margin-bottom: 32px;
+    padding-left: 11px;
+    padding-top: 3px;
+    &:hover{
+        border: 1px solid rgba(255, 255, 255, 1)
+    }
+  `;
 
 export function Technology() {
+  const [state, setState] = useState(1);
+
+  function handleClick(id: number) {
+    setState(id);
+  }
+  
+  function render(){
+    if(state === 1) return <Vehicle/>
+    if(state === 2) return <Spaceport/>
+    if(state === 3) return <Capsule/>
+  }
+
   return (
     <ContainerBackgroundImage
       url={"/technology/background-technology-desktop.jpg"}
@@ -84,17 +88,17 @@ export function Technology() {
         </SubTittleContainer>
         <ConteudoContainer>
           <ButtonContainer>
-            <RoundedButton>
+            <RoundedButton number={1} state={state} onClick={() => handleClick(1)}>
               <NumberButton>1</NumberButton>
             </RoundedButton>
-            <RoundedButton>
+            <RoundedButton number={2} state={state} onClick={() => handleClick(2)}>
               <NumberButton>2</NumberButton>
             </RoundedButton>
-            <RoundedButton>
+            <RoundedButton number={3} state={state} onClick={() => handleClick(3)}>
               <NumberButton>3</NumberButton>
             </RoundedButton>
           </ButtonContainer>
-            <Capsule/>
+          {render()}
         </ConteudoContainer>
       </MainContainer>
     </ContainerBackgroundImage>
