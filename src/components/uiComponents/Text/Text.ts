00f74842f3867c5b9cf2508b8388f48b9823e8ef
Iago Guinossi <iagoguinossi@gmail.com>
2022-11-18 12:41:02 -0300

    refactor

--- src/components/uiComponents/Text/Text.ts
+++ src/components/uiComponents/Text/Text.ts
@@ -1,8 +1,13 @@
 import styled from "styled-components";
 
-export const Text = styled.span`
+type TextProps = {
+  marginTop: number;
+}
+
+export const Text = styled.span<TextProps>`
   font-family: 'Barlow', sans-serif;
   font-size: 18px;
   color:rgba(208, 214, 249, 1);
   line-height: 32px;
+  margin-top: ${({marginTop})=> marginTop}px;
 `;
