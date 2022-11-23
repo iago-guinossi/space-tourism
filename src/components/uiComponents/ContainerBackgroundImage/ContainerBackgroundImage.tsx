704ed87c979c06c4668e114c288aa725fae844af
Iago Guinossi <iagoguinossi@gmail.com>
2022-11-23 12:03:24 -0300

    fix background size

---
+++ src/components/uiComponents/ContainerBackgroundImage/ContainerBackgroundImage.tsx
@@ -0,0 +1,41 @@
+import styled from "styled-components";
+import { useWindowSize } from "../../../hooks/useWindowSize";
+
+type ContainerProps ={
+    url: string;
+    urlTablet: string;
+    size: any;
+}
+
+  const ContainerBackground = styled.div<ContainerProps>`
+    width: 100%;
+    height: 100%;
+    background-image: url(${({url}) => url});
+    background-repeat: no-repeat;
+    background-size: ${({size}) => (size.width / size.height) > 1.6 ? '100% auto' : 'auto 100%'};
+    background-color: black;
+    box-sizing: border-box;
+    margin-top: -136px;
+    display:flex;
+    flex-direction: row;
+    justify-content: center;
+    @media(max-width: 768px){
+      background-image: url(${({urlTablet}) => urlTablet});
+      margin-top: -96px;
+      flex-direction: column;
+      align-items: center;
+      justify-content: flex-start;
+      background-size: ${({size}) => (size.width / size.height) > 0.75 ? '100% auto' : 'auto 100%'};
+    }
+    `;
+
+export function ContainerBackgroundImage({url, urlTablet, children}:any){
+  
+  const size = useWindowSize()
+  
+  return(
+    <ContainerBackground url={url} urlTablet={urlTablet} size={size}>
+      {children}
+    </ContainerBackground>
+  )
+}
