aea2be925cddcceb131bd6fece8b542dfe94332f
Iago Guinossi <iagoguinossi@gmail.com>
2022-12-01 21:39:55 -0300

    Refactor

--- src/components/pages/Home/Home.tsx
+++ src/components/pages/Home/Home.tsx
@@ -31,40 +31,10 @@ const TextContainer = styled.div`
   }
 `;
 
-const ButtonExplorer = styled.button`
-  height: 274px;
-  width: 274px;
-  background-color: #ffffff;
-  border-radius: 100%;
-  margin-bottom: 131px;
-  align-self: flex-end;
-  flex-shrink: 0;
-  &:hover {
-    background-color: #ffffff;
-  }
-  &:hover::before {
-    position: absolute;
-    width: 450px;
-    height: 450px;
-    background-color: #ffffff;
-    opacity: 0.1;
-  }
-  @media (max-width: 1000px) {
-    margin-left: 0;
-    align-self: center;
-    margin-top: 156px;
-    flex-shrink: 0;
-    height: 242px;
-    width: 242px;
-    margin-bottom: 0;
-  }
-  @media (max-width: 450px) {
-    width: 150px;
-    height: 150px;
-    margin-top: 81px;
-  }
-`;
 const LinkExplore = styled(Link)`
+  display: flex;
+  justify-content: center;
+  align-items: center;
   font-family: "Bellefair", serif;
   font-size: 32px;
   line-height: 36.67px;
@@ -79,26 +49,22 @@ const LinkExplore = styled(Link)`
   align-self: flex-end;
   flex-shrink: 0;
   box-sizing: border-box;
-  padding-top: 121px;
-  &:hover {
-    background-color: #ffffff;
-  }
-  &:hover:before {
-    position: absolute;
-    left: 0%;
-    right: 0%;
-    top: 0%;
-    bottom: 0%;
-    width: 450px;
-    height: 450px;
-    background-color: #ffffff;
-    opacity: 0.1;
+  @media (max-width: 1000px) {
+    margin-left: 0;
+    align-self: center;
+    margin-top: 156px;
+    flex-shrink: 0;
+    height: 242px;
+    width: 242px;
+    margin-bottom: 0;
   }
   @media (max-width: 450px) {
     font-size: 20px;
     line-height: 23px;
     letter-spacing: 1.25px;
-    padding-left: 22px;
+    width: 150px;
+    height: 150px;
+    margin-top: 81px;
   }
 `;
 
